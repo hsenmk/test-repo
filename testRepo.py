7f280f1432ae83ae5d20271de6400ec6090dad9b
hsenmk <153743339+hsenmk@users.noreply.github.com>
2025-06-01 22:15:46 +0300

    Added if statement

--- testRepo.py
+++ testRepo.py
@@ -2,3 +2,9 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 
 print("Hello, " + name + "! You are " + age + " years old.")
+
+if (age>18):
+    print("You are an adult.")
+else:
+    print("You are a minor.")
+    
